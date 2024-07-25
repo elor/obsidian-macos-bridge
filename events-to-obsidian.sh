082f30e68ef7c493410fb5f4d48b799e8848e9ca
Erik E. Lorenz <erik.lorenz@uni-leipzig.de>
2024-07-25 11:45:12 +0200

    rename daily note, append after Meetings header, and remove empty line

--- events-to-obsidian.sh
+++ events-to-obsidian.sh
@@ -9,7 +9,6 @@ VAULT=~/Documents/Speicherwolke/Notes/
 # Get the calendar events for today using icalBuddy
 {
   obsidian-macos-calendar-bridge
-  echo
-} | sed -i -e '/^$/r /dev/stdin' $VAULT/Daily/"$TODAY Daily Note.md"
+} | sed -i -e '/^#* Meetings$/r /dev/stdin' $VAULT/Daily/"$TODAY.md"
 
-echo "wrote events to $VAULT/Daily/$TODAY Daily Note.md"
+echo "wrote events to $VAULT/Daily/$TODAY.md"
